10ee8d4766d5457522470ef8a47ed9d4c5930a5d
shimphillip <shimphillip@gmail.com>
2022-09-19 22:49:21 -0500

    add changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
 import { useState, useEffect } from 'react';
 // import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 import axios from 'axios';
 import React from 'react';
-import Data from './data/EventData'
-import Search from './components/Search'
+import Data from './data/EventData';
+import Search from './components/Search';
 
 function App() {
   const [data, setData] = useState(null);
+  const [likes, setLikes] = useState(0);
 
   useEffect(() => {
-    axios.get("/api").then((response) => {
+    axios.get('/events').then((response) => {
       console.log(response);
-      setData(response.data.message);
+      setData(response.data.events);
     });
-  }, []);
+  }, [likes]);
 
+  const incrementLike = (name) => {
+    axios
+      .post('/like', {
+        name,
+      })
+      .then((response) => {
+        setLikes(response.data.likes);
+      });
+  };
 
   return (
-    <div className='tc bg-green ma0 pa4 min-vh-100'>
-      <Search details={Data}/>
+    <div className="tc bg-green ma0 pa4 min-vh-100">
+      {data?.map(({ _id, name, likes, duration, date }) => (
+        <div>
+          <div>{name}</div>
+          <div>
+            {likes} <button onClick={() => incrementLike(name)}>Like</button>
+          </div>
+          <div>{duration}</div>
+          <div>{date}</div>
+        </div>
+      ))}
+      <Search details={Data} />
     </div>
   );
 }
